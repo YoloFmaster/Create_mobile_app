c6b78048d8f1feb2f3793d5842ca5cfb2f711300
YoloFmaster <yolofmaste@gmail.com>
2023-09-21 20:57:12 +0300

    Lr1

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,27 @@
+fun main()
+{
+    val text = readln()
+    var new_text = ""
+    var counter = 1
+    var i = 1
+    var s = text[0]
+    for (item in text.substring(1)) {
+        i++
+        if (s != item || i == text.length) {
+            if (counter > 1) {
+                new_text += "$s$counter"
+            }
+            else
+            {
+                new_text += s
+            }
+            counter = 1
+            s = item
+        }
+        else
+        {
+            counter++
+        }
+    }
+    println(new_text)
+}
