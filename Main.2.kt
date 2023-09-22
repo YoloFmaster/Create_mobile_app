f96a84130071fe9e3696a2a9a60d621685ae3c7c
YoloFmaster <127649838+YoloFmaster@users.noreply.github.com>
2023-09-22 07:07:39 +0300

    Add files via upload

---
+++ Main.2.kt
@@ -0,0 +1,12 @@
+fun main() {
+    //Lr3
+    println("Введите сюда текст")
+    val text = readln()
+    val counterChar = mutableMapOf<Char, Int>()
+    var new_text = ""
+    text.forEach { char -> counterChar[char] = counterChar.getOrDefault(char, 0) + 1 }
+    counterChar.toSortedMap().forEach { (char, count) ->
+        new_text += "$char - $count\n"
+    }
+    println(new_text)
+}
