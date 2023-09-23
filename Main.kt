8041507da336f77c5241901ca3c6c6121ea37faa
YoloFmaster <127649838+YoloFmaster@users.noreply.github.com>
2023-09-23 19:06:33 +0300

    Update Main.kt

--- Main.kt
+++ Main.kt
@@ -1,6 +1,7 @@
 fun main() {
     //Lr3
     println("Введите натуральное число")
+    //Первый вариант
     var number = readln().toInt()
     var binaryNumber = 0
     var rank = 1
@@ -11,5 +12,9 @@ fun main() {
         binaryNumber += mod * rank
         rank *= 10
     }
+    //Второй варант
+    number = readln().toInt()
+    val binary: String = Integer.toBinaryString(number)
+    println(binary)
     println(binaryNumber)
 }
